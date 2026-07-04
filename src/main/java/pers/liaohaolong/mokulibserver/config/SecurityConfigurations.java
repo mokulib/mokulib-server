2927eea9e7574d813fa55d09404c1f07157cb1c1
廖浩龙 <aliaohaolong@gmail.com>
2026-07-05 00:17:34 +0800

    feat: 添加 security 基础配置，添加基于正则的无效登录请求过滤器

---
+++ src/main/java/pers/liaohaolong/mokulibserver/config/SecurityConfigurations.java
@@ -0,0 +1,61 @@
+package pers.liaohaolong.mokulibserver.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import pers.liaohaolong.mokulibserver.security.filter.InvalidLoginRequestFilter;
+
+@Slf4j
+@Configuration
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            InvalidLoginRequestFilter invalidLoginRequestFilter
+    ) {
+        // 关闭 CSRF 防护，因为我们使用 JWT 进行认证，不需要 CSRF 防护
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // 关闭 CORS
+        http.cors(configurer -> configurer.configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.addAllowedOrigin("*");
+            configuration.addAllowedHeader("*");
+            configuration.addAllowedMethod("*");
+            return configuration;
+        }));
+
+        // 配置授权规则(包括Mapping接口和需要权限控制的静态资源)
+        http.authorizeHttpRequests(registry -> registry
+                // 仅管理员访问 Swagger UI
+                .requestMatchers("/swagger-ui/**").hasRole("ADMIN")
+                // 其余接口放行
+                .anyRequest().permitAll()
+        );
+
+        // 禁用 session
+        http.sessionManagement(configurer -> configurer
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        // 添加过滤器
+        http.addFilterBefore(invalidLoginRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/pers/liaohaolong/mokulibserver/security/filter/InvalidLoginRequestFilter.java
@@ -0,0 +1,42 @@
+package pers.liaohaolong.mokulibserver.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pers.liaohaolong.mokulibserver.dto.ResultDTO;
+
+import java.io.IOException;
+
+import static pers.liaohaolong.mokulibserver.config.LoginFilterConfigurations.*;
+
+/**
+ * <h3>无效登录请求过滤器</h3>
+ *
+ * <p>由于两种登录请求的凭据正则互斥，因此只可能出现匹配其中一种，或者都不匹配的情况。</p>
+ */
+@Slf4j
+@Component
+public class InvalidLoginRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
+        // 是否是登录请求
+        if (LOGIN_REQUEST_MATCHER.matches(request)) {
+            // 如果是登录请求，则判断是否匹配密码登录请求或验证码登录请求
+            if (!PASSWORD_LOGIN_REQUEST_MATCHER.matches(request) && !CAPTCHA_LOGIN_REQUEST_MATCHER.matches(request)) {
+                // 如果均不匹配，则返回请求错误
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().write(ResultDTO.BAD_REQUEST.toJson());
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
