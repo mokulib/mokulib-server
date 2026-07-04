5871070f163da1310516a012b7e860113bd4e5f9
廖浩龙 <aliaohaolong@gmail.com>
2026-07-05 00:38:23 +0800

    chore: 添加注释

--- src/main/java/pers/liaohaolong/mokulibserver/service/EmailPasswordDetailsService.java
+++ src/main/java/pers/liaohaolong/mokulibserver/service/EmailPasswordDetailsService.java
@@ -5,6 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+/**
+ * <h3>邮箱密码信息加载服务</h3>
+ *
+ * @see UserDetailsService
+ */
 public interface EmailPasswordDetailsService extends UserDetailsService {
 
     @NonNull UserDetails loadUserPasswordByEmail(@NonNull String email) throws UsernameNotFoundException;
