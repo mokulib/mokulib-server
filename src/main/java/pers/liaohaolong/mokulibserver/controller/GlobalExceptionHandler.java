3b43dd6c1906c11f245ab5be9059948abe0df81f
廖浩龙 <aliaohaolong@gmail.com>
2026-07-05 23:06:40 +0800

    feat: 添加全局异常处理器，处理多种请求异常并返回统一格式的错误响应

---
+++ src/main/java/pers/liaohaolong/mokulibserver/controller/GlobalExceptionHandler.java
@@ -0,0 +1,126 @@
+package pers.liaohaolong.mokulibserver.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import pers.liaohaolong.mokulibserver.dto.ResultDTO;
+
+/**
+ * <h3>全局异常处理器</h3>
+ *
+ * <p>此处理器的优先级高于 {@link org.springframework.boot.webmvc.error.ErrorController}.</p>
+ * <p>如果不返回{@link ResponseEntity#ok(Object)}，将跳转至<code>/error</code>.</p>
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * <h3>处理请求方法不支持异常</h3>
+     *
+     * <p>
+     *     {@link HttpRequestMethodNotSupportedException} 请求方法不支持异常：表示请求方法不支持。
+     * </p>
+     * <p>不应由前端页面引起，前端无需考虑对此状态码的处理。</p>
+     *
+     * @param ignore 异常
+     * @return {@link ResultDTO}
+     */
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity<ResultDTO> handleHttpRequestMethodNotSupportedException(@NotNull Exception ignore) {
+        return ResponseEntity.ok(ResultDTO.error().message("请求方法不支持.").build());
+    }
+
+    /**
+     * <h3>处理请求错误异常</h3>
+     *
+     * <p>
+     *     {@link HttpMessageNotReadableException} 请求体不可读异常：表示请求体为空或格式错误。<br>
+     *     {@link MissingServletRequestParameterException} 缺少请求参数异常：表示请求参数缺失，由 {@link org.springframework.web.bind.annotation.RequestParam} 产生。<br>
+     *     {@link MethodArgumentTypeMismatchException} 参数类型不匹配异常：表示请求参数类型不匹配，由 {@link org.springframework.web.bind.annotation.RequestParam} 产生。
+     * </p>
+     * <p>不应由前端页面引起，前端无需考虑对此状态码的处理。</p>
+     *
+     * @param ignore 异常
+     * @return {@link ResultDTO}
+     */
+    @ExceptionHandler({
+            HttpMessageNotReadableException.class,
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class
+    })
+    public ResponseEntity<ResultDTO> handleBadRequestException(@NotNull Exception ignore) {
+        return ResponseEntity.ok(ResultDTO.error()
+                .message("请求错误.")
+                .build());
+    }
+
+    /**
+     * <h3>处理请求体校验失败异常</h3>
+     *
+     * <p>
+     *     {@link MethodArgumentNotValidException} 方法参数无效异常：表示请求体校验失败。
+     * </p>
+     *
+     * @param e 异常
+     * @return {@link ResultDTO}
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ResultDTO> handleMethodArgumentNotValidException(@NotNull MethodArgumentNotValidException e) {
+        return ResponseEntity.ok(ResultDTO.error()
+                .message(e.getBindingResult().getFieldErrors().getFirst().getDefaultMessage())
+                .data(e.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList())
+                .build());
+    }
+
+    /**
+     * <h3>处理请求参数或路径参数校验失败异常</h3>
+     *
+     * <p>
+     *     {@link HandlerMethodValidationException} 参数校验失败异常：由 Controller 的方法的参数的参数校验注解产生。
+     * </p>
+     *
+     * @param e 异常
+     * @return {@link ResultDTO}
+     */
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ResultDTO> handleHandlerMethodValidationException(@NotNull HandlerMethodValidationException e) {
+        return ResponseEntity.ok(ResultDTO.error()
+                .message(e.getParameterValidationResults().getFirst().getResolvableErrors().getFirst().getDefaultMessage())
+                .data(e.getParameterValidationResults().getFirst().getResolvableErrors().stream().map(MessageSourceResolvable::getDefaultMessage).toList())
+                .build());
+    }
+
+    /**
+     * <h3>处理不应出现的异常</h3>
+     * <p>
+     *     {@link DataAccessException} 数据访问异常：表示 MySQL 数据库操作异常。<br>
+     *     {@link Exception} 未知异常
+     * </p>
+     *
+     * @param e 异常
+     * @return {@link ResultDTO}
+     */
+    @ExceptionHandler({
+            DataAccessException.class,
+            Exception.class})
+    public ResponseEntity<ResultDTO> handleUncontrollableException(@NotNull Exception e) {
+        log.warn("Ah! T_T\n", e);
+        return ResponseEntity.ok(ResultDTO.error()
+                .message("服务器错误，请稍后重试.")
+                .data(e.getMessage())
+                .build());
+    }
+
+}
