bbe0563bc9ef16cca63f82b062578632ff4c99c7
廖浩龙 <aliaohaolong@gmail.com>
2026-07-06 21:58:48 +0800

    test: 添加一些数据初始化语句
    - 经实测，该文件内容不允许为空

---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `user` (`email`, `password`, `role`, `username`, `is_deleted`, `create_time`, `delete_time`)
+VALUES ('mr_wang@moku.com', '$2a$10$59NxOB9SYqjT/gw28XKDSuoOb/OaOUO7UShppKh1b3EDrS.ewmha2', 'ADMIN', 'Mr_Wang', true, '2026-05-01T10:00:00', '2026-07-01T10:00:00');
+INSERT INTO `user` (`email`, `password`, `role`, `username`, `is_deleted`)
+VALUES ('zhang@moku.com', '$2a$10$59NxOB9SYqjT/gw28XKDSuoOb/OaOUO7UShppKh1b3EDrS.ewmha2', 'ADMIN', 'Zhang', false);
+INSERT INTO `user` (`email`, `password`, `role`, `username`, `is_deleted`)
+VALUES ('mr_wang@moku.com', '$2a$10$59NxOB9SYqjT/gw28XKDSuoOb/OaOUO7UShppKh1b3EDrS.ewmha2', 'ADMIN', 'Mr_Wang', false);
