6716cbdcf1d9ffa8c6b13ad663f74512b2df2b33
廖浩龙 <aliaohaolong@gmail.com>
2026-08-10 12:54:38 +0800

    refactor: 重构为 IService 实现

--- src/main/java/pers/liaohaolong/mokulibserver/service/business/CategoryService.java
+++ src/main/java/pers/liaohaolong/mokulibserver/service/business/CategoryService.java
@@ -1,11 +1,12 @@
 package pers.liaohaolong.mokulibserver.service.business;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import pers.liaohaolong.mokulibserver.exception.BusinessException;
 import pers.liaohaolong.mokulibserver.model.Category;
 
 import java.util.List;
 
-public interface CategoryService {
+public interface CategoryService extends IService<Category> {
 
     Category add(String name);
 
--- src/main/java/pers/liaohaolong/mokulibserver/service/business/impl/CategoryServiceImpl.java
+++ src/main/java/pers/liaohaolong/mokulibserver/service/business/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package pers.liaohaolong.mokulibserver.service.business.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,13 @@ import java.util.List;
 @Slf4j
 @Service
 @AllArgsConstructor
-public class CategoryServiceImpl implements CategoryService {
-
-    private final CategoryMapper categoryMapper;
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 
     @Override
     @Transactional
     public Category add(String name) {
         // 是否已存在
-        Category category = categoryMapper.selectOne(new LambdaQueryWrapper<Category>().eq(Category::getName, name));
+        Category category = getOne(new LambdaQueryWrapper<Category>().eq(Category::getName, name));
         // 已存在则返回
         if (category != null)
             return category;
@@ -33,23 +32,23 @@ public class CategoryServiceImpl implements CategoryService {
         category.setName(name);
 
         // 插入并返回
-        categoryMapper.insert(category);
+        save(category);
         return category;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Category> getAll() {
-        return categoryMapper.selectList(null);
+        return list();
     }
 
     @Override
     @Transactional(readOnly = true)
     public Category get(Integer id) throws BusinessException {
-        if (!categoryMapper.exists(new LambdaQueryWrapper<Category>().eq(Category::getId, id)))
+        if (!exists(new LambdaQueryWrapper<Category>().eq(Category::getId, id)))
             throw new BusinessException("分类未找到");
 
-        return categoryMapper.selectById(id);
+        return getById(id);
     }
 
 }
