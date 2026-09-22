4c359810c0f1923f854085806ca20140b2f2edf8
廖浩龙 <aliaohaolong@gmail.com>
2026-09-22 21:40:47 +0800

    perf: 调整排行榜返回数量至 5

--- src/main/java/pers/liaohaolong/mokulibserver/dao/virtual/RankMapper.java
+++ src/main/java/pers/liaohaolong/mokulibserver/dao/virtual/RankMapper.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public interface RankMapper {
 
-    @Select("SELECT book_id FROM borrow_record LEFT JOIN book_copy ON borrow_record.book_copy_id = book_copy.id GROUP BY book_copy.book_id ORDER BY COUNT(*) DESC LIMIT 10")
+    @Select("SELECT book_id FROM borrow_record LEFT JOIN book_copy ON borrow_record.book_copy_id = book_copy.id GROUP BY book_copy.book_id ORDER BY COUNT(*) DESC LIMIT 5")
     List<Integer> borrow();
 
-    @Select("SELECT book_id FROM favorite GROUP BY book_id ORDER BY COUNT(*) DESC LIMIT 10")
+    @Select("SELECT book_id FROM favorite GROUP BY book_id ORDER BY COUNT(*) DESC LIMIT 5")
     List<Integer> favorite();
 
-    @Select("SELECT book_id FROM book_copy GROUP BY book_id HAVING MIN(create_time) >= NOW() - INTERVAL 30 DAY ORDER BY MAX(create_time) DESC LIMIT 10")
+    @Select("SELECT book_id FROM book_copy GROUP BY book_id HAVING MIN(create_time) >= NOW() - INTERVAL 30 DAY ORDER BY MAX(create_time) DESC LIMIT 5")
     List<Integer> newMonthly();
 
-    @Select("SELECT book_id FROM book_copy GROUP BY book_id ORDER BY MAX(create_time) DESC LIMIT 10")
+    @Select("SELECT book_id FROM book_copy GROUP BY book_id ORDER BY MAX(create_time) DESC LIMIT 5")
     List<Integer> newStore();
 
 }
